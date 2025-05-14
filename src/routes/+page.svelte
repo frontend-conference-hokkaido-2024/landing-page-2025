238b7afdb97fc573539fef4510335392d5343370
kengo2003 <saitokengo2003@gmail.com>
2025-05-14 22:19:29 +0900

    仮設ページ作成

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,59 +1,32 @@
-<script lang="ts">
-	import Counter from '../lib/components/features/Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcomeFallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>フロントエンドカンファレンス北海道2025</title>
+	<meta name="description" content="フロントエンドカンファレンス北海道2025" />
 </svelte:head>
 
-<section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset={welcome} type="image/webp" />
-				<img src={welcomeFallback} alt="Welcome" />
-			</picture>
-		</span>
+<section class="p-4">
+	<h1 class="text-2xl font-bold pt-3 pb-5">フロントエンドカンファレンス北海道2025</h1>
 
-		to your new<br />SvelteKit app
-	</h1>
+	<div class="py-6">
+		<p>2025/09/06</p>
+		<p>エア・ウォーターの森</p>
+		<p>北海道札幌市中央区北8条西13丁目28-21</p>
+	</div>
 
-	<h2>
-		try editing <strong>src/routes/+page.svelte</strong>
-	</h2>
-
-	<Counter />
+	<div>
+		<a href="https://x.com/fec_hokkaido" class="underline pr-3">X</a>
+		<a href="https://note.com/fec_hokkaido/m/m88550dbf59dd" class="underline">note</a>
+	</div>
+	<div class="py-6">
+		<a
+			href="https://esa-pages.io/p/sharing/22435/posts/13/3a8f7d0dddaeb2ea656e.html"
+			class="underline pr-3">行動規範</a
+		>
+		<a
+			href="https://aback-jasmine-06b.notion.site/cf184497a6414a97aea49c1f1b2f5b5f"
+			class="underline"
+		>
+			プライバシーポリシー
+		</a>
+	</div>
+	<a href="https://www.frontend-conf.jp/" class="underline">2024</a>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
